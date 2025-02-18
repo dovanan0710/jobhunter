027e0beb2c09219e8375ac39f05c532bcbdad394
Do Van An <an07102003@gmail.com>
2025-02-18 08:43:29 +0700

    ResponseEntity

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -3,9 +3,12 @@ package vn.hoidanit.jobhunter.controller;
 import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,36 +25,43 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user")
-    public User createNewUser(@RequestBody User postManUser) {/// truyền dữ liệu mới dùng requestbody
-
+    @PostMapping("/users")
+    public ResponseEntity<User> createNewUser(@RequestBody User postManUser) {
         User ericUser = this.userService.handleCreteUsser(postManUser);
-        return ericUser;
+        return ResponseEntity.status(HttpStatus.CREATED).body(ericUser);
     }
 
-    @DeleteMapping("/user/{id}") // truyền độg tham số {}
-    public String deleteUser(@PathVariable("id") long id) {
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
+
+        if (id >= 1500) {
+            throw new IdInvalidException("Id không lớn hơn 1500");// phản hồi ở terminal
+        }
         this.userService.handleDeleteUser(id);
-        return "ericusser";
+        return ResponseEntity.ok("ericUser");
+        // return ResponseEntity.status(HttpStatus.OK).body("ericUser");
     }
 
-    @GetMapping("/user") // lấy tất cả user
-    public List<User> getAllUser() {
-        return userService.fetchAllUser();
+    // fetch user by id
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        User fetchUser = this.userService.fetchUserById(id);
+        // return ResponseEntity.ok(fetchUser);
+        return ResponseEntity.status(HttpStatus.OK).body(fetchUser);
     }
 
-    @GetMapping("/user/{id}") // lấy 1 user
-    public User getUserById(@PathVariable("id") long id) {
-
-        return userService.fetchUserById(id);
-
+    // fetch all users
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUser() {
+        // return ResponseEntity.ok(this.userService.fetchAllUser());
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUser());
     }
 
-    @PutMapping("user")
-    public User updatUser(@RequestBody User user) {
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
         User ericUser = this.userService.handleUpdateUser(user);
-        return ericUser;
+        return ResponseEntity.ok(ericUser);
     }
-    // công cụ viết nhanh CRUD spring data rest dùng cho dự án đơn giản
 
 }
